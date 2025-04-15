a8d2d3841a36259fe28455cde2223952363e6557
eliasecchig <115624100+eliasecchig@users.noreply.github.com>
2025-04-15 18:33:18 +0200

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,37 +1,17 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
+# No user-configurable parameters
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-  idx.internal.templates-cli.enable = true;
+  # Shell script that produces the final environment
+  bootstrap = ''
+    # Copy the folder containing the `idx-template` files to the final
+    # project folder for the new workspace. ${./.} inserts the directory
+    # of the checked-out Git folder containing this template.
+    cp -rf ${./.} "$out"
 
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.python311
-    pkgs.python311Packages.pip
-  ];
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "ms-toolsai.jupyter"
-      "ms-python.python"
-    ];
-    workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-        create-venv = ''
-        python -m venv .venv
-        source .venv/bin/activate
-        pip install agent-starter-pack uv
-        '';
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ];
-      };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
-    };
-    # Enable previews and customize configuration
-    previews = {};
-  };
+    # Set some permissions
+    chmod -R +w "$out"
+
+    # Remove the template files themselves and any connection to the template's
+    # Git repository
+    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+  '';
 }
